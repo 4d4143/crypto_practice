98c5e27937e87208ccf1e3f939867621a27d611e
4d4143 <manuelalejandrocamino@gmail.com>
2021-02-06 19:22:57 -0300

    finished Affine cipher section

--- Cracking Codes/Chapter14/affineCipher.py
+++ Cracking Codes/Chapter14/affineCipher.py
@@ -10,12 +10,12 @@ SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz12345\
 
 
 def main():
-    myMessage = """"A computer would deserve to be called intelligent
-    if it could deceive a human into believing that it was human."
-    -Alan Turing"""
+    myMessage = """5QG9ol3La6QI93!xQxaia6faQL9QdaQG1!!axQARLa!!AuaRL
+QQQQADQALQG93!xQxaGaAfaQ1QX3o1RQARL9Qda!AafARuQLX1LQALQI1iQX3o1RN"
+QQQQ-5!1RQP36ARu"""
 
     myKey = 2894
-    myMode = 'encrypt'
+    myMode = 'decrypt'
 
     if myMode == 'encrypt':
         translated = encryptMessage(myKey, myMessage)
@@ -34,16 +34,62 @@ def getKeyParts(key):
     return (keyA, keyB)
 
 
-def checkKeys():
+def checkKeys(keyA, keyB, mode):
+    alphabetLength = len(SYMBOLS) - 1
+
+    if keyA == 0 and mode == 'encrypt':
+        sys.exit('Cipher is weak if key A is 1.\
+             Please choose a different value.')
+    if keyB == 0 and mode == 'encrypt':
+        sys.exit('Cipher is weak if key B is 0. Choose a different key.')
+    if keyA < 0 or keyB < 0 or keyB > alphabetLength:
+        sys.exit('Key A must be greater than 0 and key B must be between\
+            0 and {}'.format(alphabetLength))
+    if cryptoMath.gcd(keyA, alphabetLength) != 1:
+        sys.exit('Key A ({}) and the symbol set size ({}) are not\
+            relatively prime. Choose a different key.'.format(
+                keyA,
+                alphabetLength))
 
 
-def encryptMessage():
+def encryptMessage(key, message):
+    lenSymbols = len(SYMBOLS)
+    keyA, keyB = getKeyParts(key)
+    checkKeys(keyA, keyB, 'encrypt')
+    cipherText = ''
+    for symbol in message:
+        if symbol in SYMBOLS:
+            # Encrypt the symbol
+            symbolIndex = SYMBOLS.find(symbol)
+            cipherText += SYMBOLS[(symbolIndex * keyA + keyB) % lenSymbols]
+        else:
+            cipherText += symbol  # just append the symbol
+    return cipherText
 
 
-def decryptMessage():
+def decryptMessage(key, message):
+    lenSymbols = len(SYMBOLS)
+    keyA, keyB = getKeyParts(key)
+    checkKeys(keyA, keyB, 'decrypt')
+    plainText = ''
+    modInverseOfA = cryptoMath.findModInverse(keyA, len(SYMBOLS))
+
+    for symbol in message:
+        if symbol in SYMBOLS:
+            symbolIndex = SYMBOLS.find(symbol)
+            plainText += SYMBOLS[(symbolIndex - keyB) *
+                                 modInverseOfA % lenSymbols]
+        else:
+            plainText += symbol
+    return plainText
 
 
 def getRandomKey():
+    while True:
+        keyA = random.randint(2, len(SYMBOLS))
+        keyB = random.randint(2, len(SYMBOLS))
+        if cryptoMath.gcd(keyA, len(SYMBOLS)) == 1:
+            return keyA * len(SYMBOLS) + keyB
 
 
 if __name__ == '__main__':
--- Cracking Codes/Chapter14/cryptoMath.py
+++ Cracking Codes/Chapter14/cryptoMath.py
@@ -23,7 +23,7 @@ def findModInverse(a, m):
     while v3 != 0:
 
         q = u3 // v3
-        v1, v2, v3, u1, u2, u3 = (u1 - q*v1), (u2 - q*v2), (u3 - q*v3)\
-            , v1, v2, v3
+        v1, v2, v3, u1, u2, u3 = (u1 - q*v1), (u2 - q*v2), (u3 - q*v3),\
+            v1, v2, v3
 
     return u1 % m
